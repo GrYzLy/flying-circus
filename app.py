1b6bf1496cdf04f883da162a9ac5b77a754d292b
Lukasz <my.email>
2021-03-07 08:39:02 +0100

    Backend: 'Silnik' testu języka angielskiego,  front: update result.html i test.html

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 app.secret_key = 'NOT_SO_SECRET'
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -33,21 +34,52 @@ def logout():
 
     return render_template('index.html')
 
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    if not is_logged_in():
+        return redirect(url_for('index'))
+
+    question = list(data.questions.items())[session['question_count']]
+    return render_template('test.html', question=question)
+
+
+@app.route('/submit_question', methods=['POST'])
+def submit_question():
+    if not is_logged_in():
+        return redirect(url_for('index'))
+    question = list(data.questions.items())[session['question_count']]
+    answers = list(question[1].items())
+
+    if 'answer' in request.form:
+        try:
+            converted_answer = int(request.form['answer'])
+        except ValueError:
+            pass
+        else:
+            if answers[converted_answer][1]:
+                session['points'] += 1
+
+    session['question_count'] += 1
+
+    if session['question_count'] > 4:
+        return redirect(url_for('result'))
+
+    return redirect(url_for('test'))
+
 @app.route('/result')
 def result():
+    if not is_logged_in():
+        return redirect(url_for('index'))
+
     return render_template('result.html')
 
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
-
-
+# helper methods
 def has_authenticated():
     return is_existing_user() and is_password_ok()
 
 
-# helper methods
 def is_existing_user():
     return request.form['email'] in data.users
 
@@ -58,6 +90,9 @@ def is_password_ok():
 
     return bcrypt.checkpw(received_password, actual_password)
 
+
+def is_logged_in():
+    return 'email' in session
 
 def set_session_data():
     session['email'] = request.form['email']
